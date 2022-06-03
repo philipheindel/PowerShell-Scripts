b13b6be4dba9147d67478f6665bd4fcd80fb7903
Philip Heindel <p.e.heindel@gmail.com>
2022-06-02 21:16:58 -0400

    Create GUIFunctions-Test.ps1

---
+++ modules/GUIFunctions-Test.ps1
@@ -0,0 +1,12 @@
+Add-Type -assembly System.Windows.Forms
+
+Import-Module -Name C:\Users\Philip\Desktop\ps\GUIFunctions.psm1
+
+$mainForm = New-Object System.Windows.Forms.Form
+
+$newButton = New-Component -type "Button" -text "Test Button" -x 20 -y 100 -w 150 -h 50 -parent $mainForm
+$newLabel = New-Component -type "label" -text "LabeL!!!" -parent $mainForm
+
+$mainForm.FormBorderStyle = 2
+
+$mainForm.ShowDialog()
