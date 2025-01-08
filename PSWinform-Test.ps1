0fe80bdef0b81f02838d03ba03b95c96a4a93fe1
Philip Heindel <p.e.heindel@gmail.com>
2025-01-07 21:33:12 -0500

    Added test file and some updates to the winform module

---
+++ PSWinform-Test.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module -Name .\PSWinform.psm1 -Force
+
+$frm = New-Form -Name "Test form" -Width "300"
+
+Show-Form -Form $frm
--- PSWinform.psm1
+++ PSWinform.psm1
@@ -7,12 +7,17 @@ public static extern IntPtr GetConsoleWindow();
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
 
+[System.Windows.Forms.Form]$private:FormSingleton = $null
+
 function New-Form {
     <#
     .SYNOPSIS
         Initializes a new form object.
 
     .DESCRIPTION
+
+    .PARAMETER Name
+        The name used to identify the form object. Default = Form1.
 
     .PARAMETER Text
         The window text displayed at the top of the form. Default = Form1.
@@ -22,6 +27,9 @@ function New-Form {
 
     .PARAMETER DisableVisualStyles
         Causes EnableVisualStyles() to not be called which is called by default.
+
+    .PARAMETER AutoScaleMode
+        Sets the AutoScaleMode of the form. Default = Font.
 
     .INPUTS
         This function does not accept any piped inputs.
@@ -33,20 +41,84 @@ function New-Form {
     [CmdletBinding(DefaultParameterSetName = "Form")]
     [OutputType([System.Windows.Forms.Form])]
     Param(
-        [Parameter(ParameterSetName = "Form", Position = 0)]
+        [Parameter(ParameterSetName = "Form", 
+                   Position = 0,
+                   Mandatory)]
+        [System.String]$Name = "Form1",
+        [Parameter(ParameterSetName = "Form", 
+                   Position = 0)]
         [System.String]$Text = "Form1",
-        [Parameter(ParameterSetName = "Form", Position = 1)]
+        [Parameter(ParameterSetName = "Form", 
+                   Position = 1)]
+        [Alias("ClientWidth")]
         [System.Int32]$Width = 200,
-        [Parameter(ParameterSetName = "Form", Position = 2)]
+        [Parameter(ParameterSetName = "Form", 
+                   Position = 2)]
+        [System.Windows.Forms.AutoScaleMode]$AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font,
+        [Parameter(ParameterSetName = "Form", 
+                   Position = 3)]
+        [System.Management.Automation.SwitchParameter]$AutoScroll,
+        [Parameter(ParameterSetName = "Form", 
+                   Position = 100)]
         [System.Management.Automation.SwitchParameter]$DisableVisualStyles
     )
-    process {
+    begin {
         if (-not $DisableVisualStyles) {
             [System.Windows.Forms.Application]::EnableVisualStyles()
         }
 
+        if ($Name -ne "Form1" -and $Text -eq "Form1") {
+            $formText = $Name
+        } else {
+            $formText = $Text
+        }
+
+
+    }
+    process {
+        
+
         [System.Windows.Forms.Form]$newForm = New-Object -TypeName System.Windows.Forms.Form
+
+        $newForm.Name = $Name
+        $newForm.Text = $formText
+        $newForm.ClientSize = New-Object -TypeName System.Drawing.Size($Width, 0)
+        $newForm.AutoScaleMode = $AutoScaleMode
+        
+        
 
         Write-Output -InputObject $newForm
     }
+    end {
+        Write-Output
+    }
 }
+
+function Show-Form {
+    <#
+    .SYNOPSIS
+        Shows the provided form as a dialog window.
+
+    .DESCRIPTION
+
+    .PARAMETER Form
+        The form to show.
+
+    .INPUTS
+        The Form parameter can be passed as a pipeline input. 
+    #>
+    [Alias("sfrm")]
+    [CmdletBinding()]
+    Param(
+        [Parameter(ParameterSetName = "a", 
+                   Position = 0, 
+                   Mandatory, 
+                   ValueFromPipeline)]
+        [System.Windows.Forms.Form]$Form
+    )
+
+    $Form.ShowDialog()
+}
+
+Export-ModuleMember -Function New-Form -Alias @("Initialize-Form", "frm")
+Export-ModuleMember -Function Show-Form -Alias @("sfrm")
